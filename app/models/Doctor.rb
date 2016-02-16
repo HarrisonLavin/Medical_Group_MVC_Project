71094e83ed0c87baad707a86183082189e0b64d0
HarrisonLavin <harrison.lavin@gmail.com>
2016-02-16 13:37:14 -0500

    fills out doctor.rb

---
+++ app/models/Doctor.rb
@@ -0,0 +1,31 @@
+class Doctor
+
+  attr_reader :name, :age, :specialties
+  @@all
+
+  def initialize(name)
+    @name = name
+    @age = 0
+    @specialties = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_specialty(spec)
+    @specialties << spec
+  end
+
+  def appointments
+    Appointments.all.find_all do |appt|
+      appt.docotr == self
+    end
+  end
+
+  def patients
+    self.appointments.map {|appt| appt.patient}
+  end
+
+end
